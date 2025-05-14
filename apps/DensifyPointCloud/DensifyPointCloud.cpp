86596b064fbb43e35ec32b62e0aeb06242f0cc47
Francisco <francisco.raverta@gmail.com>
2025-05-14 19:03:26 -0300

    Testing about using segmentation

--- apps/DensifyPointCloud/DensifyPointCloud.cpp
+++ apps/DensifyPointCloud/DensifyPointCloud.cpp
@@ -402,8 +402,9 @@ int main(int argc, LPCTSTR* argv)
 	if (OPT::thFilterPointCloud < 0) {
 		// filter point-cloud based on camera-point visibility intersections
 		LOG("Filtering stage - FRAN");
-		LOG("PointCloudSegmentations", scene.pointcloud.segmentations);
+		LOG("PointCloudSegmentations", scene.pointcloud.segmentations[0]);
 		scene.PointCloudFilter(OPT::thFilterPointCloud);
+		LOG("PointCloudSegmentations2", scene.pointcloud.segmentations[0]);
 		const String baseFileName(MAKE_PATH_SAFE(Util::getFileFullName(OPT::strOutputFileName))+_T("_filtered"));
 		scene.Save(baseFileName+_T(".mvs"), (ARCHIVE_TYPE)OPT::nArchiveType);
 		LOG(baseFileName+_T(".ply"));
