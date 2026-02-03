4bf880b93706f7a3cd81677299dfb4518f209163
FranciscoRaverta <francisco.raverta@gmail.com>
2026-02-03 12:26:55 +0100

    Added changes to get the covariance from the SfM

--- libs/MVS/Interface.h
+++ libs/MVS/Interface.h
@@ -440,7 +440,7 @@ struct Interface
 			template <typename MAT, typename POS>
 			Pose(const MAT& _R, const POS& _C) : R(_R), C(_C) {}
 			template <typename MAT, typename POS, typename MATC>
-			Pose(const MAT& _R, const POS& _C, const MATC& _Cov) : R(_R), C(_C) Cov(_Cov) {}
+			Pose(const MAT& _R, const POS& _C, const MATC& _Cov) : R(_R), C(_C), Cov(_Cov) {}
 
 			// translation vector t = -RC
 			inline Pos3d GetTranslation() const { return R*(-C); }
