5a949c37f17e2f5c75ee89068e5c4fac4a263431
FranciscoRaverta <francisco.raverta@gmail.com>
2026-02-04 13:04:25 +0100

    Added changes to get the covariance from the SfM

--- libs/MVS/Image.cpp
+++ libs/MVS/Image.cpp
@@ -142,12 +142,7 @@ bool Image::ReadConfidenceImage(IMAGEPTR pConfidenceImage, Image32F& image)
     }
 
     image.create(temp.rows, temp.cols);
-
-    const int total = image.rows * image.cols;
-    for (int i = 0; i < total; ++i) {	
-		image.data[i] = float(temp.data[i]) / 100.0f;
-		std::cout << "FRAN - Print confidence pixel " << i << ": " << image.data[i] << std::endl;
-	}
+	image.data = float(temp.data) / 100.0f;
 
     return true;
 } // ReadImage
