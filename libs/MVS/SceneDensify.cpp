7e014fcb52de12c88abfd8b0875ef42ec3d88a91
FranciscoRaverta <francisco.raverta@gmail.com>
2026-02-17 12:43:16 +0100

    Adding a probability estimation for the label plus uncertainty estimation

--- libs/MVS/SceneDensify.cpp
+++ libs/MVS/SceneDensify.cpp
@@ -1746,7 +1746,7 @@ void DepthMapsData::FuseDepthMaps(PointCloud& pointcloud, bool bEstimateColor, b
 				segmentationFrequency.clear();
 			}
 		}
-		bool applyDenseCRF = false;
+		bool applyDenseCRF = true;
 		if(applyDenseCRF) 
 		{
 			std::vector<PointXYZRGB> crf_points(pointcloud.points.size());
