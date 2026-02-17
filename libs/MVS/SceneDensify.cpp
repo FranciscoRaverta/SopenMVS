8e710538fa676a4f9232cb671dfb58e4c6de3748
FranciscoRaverta <francisco.raverta@gmail.com>
2026-02-17 11:29:52 +0100

    Adding a probability estimation for the label plus uncertainty estimation

--- libs/MVS/SceneDensify.cpp
+++ libs/MVS/SceneDensify.cpp
@@ -1726,10 +1726,10 @@ void DepthMapsData::FuseDepthMaps(PointCloud& pointcloud, bool bEstimateColor, b
 						//	Z += exp(v - maxLog);
 						//float prob = exp(bestVal - maxLog)/Z;
 						float prob = std::exp(bestVal);
-						std::vector<float> probabs;
-						for(int c=0;c<numLabels;c++)
-							probabs[c] += std::exp(sumLogProbs[c]);
-						per_point_probabilities.emplace_back(probabs);
+						//std::vector<float> probabs;//FRAN
+						// for(int c=0;c<numLabels;c++)
+						// 	probabs[c] += std::exp(sumLogProbs[c]);
+						// per_point_probabilities.emplace_back(probabs);
 
 						//pointcloud.segmentations.emplace_back(modeColor);
 						pointcloud.segmentations.emplace_back(bestLabel);
