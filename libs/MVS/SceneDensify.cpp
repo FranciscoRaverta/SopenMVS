0125aa1a58eb48fb08846d2d02b49e4e6022fc15
FranciscoRaverta <francisco.raverta@gmail.com>
2026-03-06 11:56:07 +0100

    Adding a probability estimation for the label plus uncertainty estimation

--- libs/MVS/SceneDensify.cpp
+++ libs/MVS/SceneDensify.cpp
@@ -1476,7 +1476,7 @@ void DepthMapsData::ApplyDenseCRF3D(
     }
 }
 
-SEACAVE::Matrix3x3d PoseCovarianceEstimation(Camera& camera, double depth, SEACAVE::TPoint2 point2d, SEACAVE::CovMatrix C_pose)
+SEACAVE::Matrix3x3d PoseCovarianceEstimation(Camera& camera, double depth, SEACAVE::Point2f point2d, SEACAVE::CovMatrix C_pose)
 {
 	KMatrix K = camera.K;
 	RMatrix R = camera.R;
@@ -1489,7 +1489,7 @@ SEACAVE::Matrix3x3d PoseCovarianceEstimation(Camera& camera, double depth, SEACA
 
 	// J = [J_t J_R J_d J_u] with X = R^T d K^-1 u_h + t , where d is depth, u_h is the expanded (u,v,1) vector 
 	// Compute J_t = dX/dt = I_3x3
-	J.block<3,3>(0,0) = SEACAVE::Matrix3x3d::Identity();
+	J.block<3,3>(0,0) = SEACAVE::Matrix3x3d::IDENTITY;
 
 	// Compute J_R = dX/dR = 
 	SEACAVE::Vec3d Y = R.t() * depth * K.inv() * u_h;
